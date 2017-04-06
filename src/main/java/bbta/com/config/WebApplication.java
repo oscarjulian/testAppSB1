a96dd46af8c0a39f43857fd25642ad06b8965247
ocampi1 <ocampi1@192.168.3.224>
2017-04-06 11:54:44 -0500

    ComponentScan Annotation

--- src/main/java/bbta/com/config/WebApplication.java
+++ src/main/java/bbta/com/config/WebApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"bbta.com.controller"})
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
